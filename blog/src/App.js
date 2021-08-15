f3d0b28349cf6c53f57f948b5dd14e7f6d817abe
mujjing <junji636@gmail.com>
2021-08-15 19:22:30 +0900

    useState 작성

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
   let posts = '강남고기맛집';
 
   return (
@@ -10,7 +12,11 @@ function App() {
       <div className="black_nav">
         <div style={ { color:'yellow', fontSize : '30px' } }>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className="list">
+        <h3>{ 글제목[0] }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
